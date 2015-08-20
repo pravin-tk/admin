0ae7943d1b57ba2c4299e0963afc8111867f000c
pravin-tk <pravin.jadhav@housing.com>
2015-08-20 12:02:48 +0530

    Adding standard name to school list API

--- src/main/java/org/school/admin/dao/SchoolSearchImpl.java
+++ src/main/java/org/school/admin/dao/SchoolSearchImpl.java
@@ -176,10 +176,10 @@ public class SchoolSearchImpl {
 				+ " s.schoolCategory as schoolCategory,s.schoolClassification as schoolClassification,"
 				+ " s.schoolType as schoolType,s.rating as rating,s.galeryImages as galeryImages,s.reviews as reviews, "
 				+ distance + " as distance,ci.totalFee as totalFee,ci.vacantSeat as seats,"
-				+ " ci.standardType.id as standardId, ta.name as teachingApproach,"
+				+ " ci.standardType.id as standardId, st.name as standardName, ta.name as teachingApproach,"
 				+ " COALESCE( (SELECT con.mobileNo FROM ContactInfo con WHERE s.schoolId = con.school.id AND con.isPrimary = 1 AND con.type = 1), '') as primaryContactNo "
 				+ isShortlistedQuery
-				+ " FROM SchoolSearch s, School ss JOIN ss.classInfos ci"
+				+ " FROM SchoolSearch s, School ss JOIN ss.classInfos ci JOIN ci.standardType st"
 				+ " JOIN ss.schoolMediums sm JOIN ci.teachingApproachType ta" + queryJoin
 				+ " WHERE s.schoolId = ss.id "
 				+ queryCondition+ " Group By ss.id"+finalOrder
